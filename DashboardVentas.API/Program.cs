fadfb1e8f5bb34ff4596e9a1c74fb1d60d27fd96
jose <131331940+joseh2@users.noreply.github.com>
2026-06-29 23:35:32 -0600

    Update Program.cs
    migraciones

--- DashboardVentas.API/Program.cs
+++ DashboardVentas.API/Program.cs
@@ -38,5 +38,10 @@ app.UseCors("react");
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
